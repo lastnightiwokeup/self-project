1e795a95c4bddec89db310c8f5f5ffff5be47416
LEUNG, Connie TK [ITS] <connie-tk.leung@polyu.edu.hk>
2024-03-26 15:34:31 +0800

    update db to postgreSQL

--- backend/database/base.sql
+++ backend/database/base.sql
@@ -1,4 +1,5 @@
--- create budget item table 
+--MySQL--
+
 CREATE TABLE BudgetItem (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     itemName VARCHAR(255) CHARACTER SET UTF8MB4 NOT NULL,
@@ -7,4 +8,16 @@ CREATE TABLE BudgetItem (
     category VARCHAR(255) CHARACTER SET UTF8MB4 NOT NULL,
     createdAt datetime NOT NULL,
     updatedAt datetime NOT NULL,
+);
+
+--PostgreSQL--
+
+CREATE TABLE BudgetItem (
+    id SERIAL PRIMARY KEY,
+    itemName VARCHAR(255) NOT NULL,
+    amount INTEGER NOT NULL,
+    date VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
+    updatedAt TIMESTAMP NOT NULL
 );
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,19 @@
-import express, { Request, Response } from 'express';
-import mysql2 from 'mysql2/promise';
+import express from 'express';
+import { Pool } from 'pg';
 import cors from 'cors';
 import budgetItemRouter from './budget_tracker/routes/controllers/api/budgetItem';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-const db = mysql2.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Lauren0627",
-  database: "budget_tracker",
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
-
 // to send from html body
 app.use(express.json());
 app.use(cors());
